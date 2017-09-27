96066a5fec4e014b09459a89096bcfd477158e65
kreaux77@gmail.com <fedja.belusov@inbox.lv>
2017-09-27 14:08:43 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#3.piemersNAME="Vards Uzvards
+echo $NAME
+unset NAME
+echo $NAME
+
+#2.piemers
+#NAME="Vards Uzvards"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvards Vards"
+#echo $NAME
+
+#1.piemers
+#NAME="Vards Uzvards"
+#echo $NAME
+
+#0.piemers
+history > history_20170927.txt
+
